0498a8d653d40a2e5c10e5654c6c56a5b30f6fb0
LisePetculescu <lise.lili97gmail.com>
2026-02-05 12:05:54 +0100

    text file parser

---
+++ lesson-02/txt-file-parser/script.js
@@ -0,0 +1,46 @@
+import { readFile } from 'fs/promises';
+
+// to run it, go to 'txt-file-parser' folder in terminal and run "node script.js"
+
+const fileName = 'danish_companies.txt'
+async function start() {
+
+    const textToParse = await fetchFile('danish_companies.txt')
+
+    console.log(parseFile(textToParse));
+}
+
+
+async function fetchFile(file) {
+    try {
+        const textFromFile = await readFile(file, 'utf8');
+        // console.log(textFromFile);
+        return textFromFile;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+function parseFile(textToParse) {
+
+    const rows = textToParse.split('~').filter(row => row.trim());
+    const parsedFile = rows.map(row => {
+        const fields = row.split('|');
+        return {
+            CVR: fields[0]?.trim(),
+            CompanyName: fields[1]?.trim(),
+            Street: fields[2]?.trim(),
+            Number: fields[3]?.trim(),
+            PostalCode: fields[4]?.trim(),
+            Town: fields[5]?.trim(),
+            EmailAddress: fields[6]?.trim(),
+        };
+    });
+    
+    // return parsedFile;  
+    return JSON.stringify(parsedFile, null, 2); 
+
+}
+
+start()
